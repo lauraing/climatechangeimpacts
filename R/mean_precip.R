2385d329bd2e8523276a72b19dcf54d0afc6bb9d
Keene Morrow <keene.morrow@gmail.com>
2020-03-11 11:50:46 -0700

    Thoughts on meran_precip functionality

--- R/mean_precip.R
+++ R/mean_precip.R
@@ -1,1 +1,4 @@
 # function to calculate mean precipitation
+
+# It would be cool if we made it so the data didn't have to have an entry for every day.
+# Maybe this would mean that the function would recognize what period of time the data takes place over and calculate the number of days in that period, then caluclate the mean as (sum of rainfall)/(end_date - start_date)
