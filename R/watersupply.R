324ff9d658a2388a208060947fb5dd274ea40425
Keene Morrow <keene.morrow@gmail.com>
2020-03-14 17:16:21 -0700

    watersupply complete

--- R/watersupply.R
+++ R/watersupply.R
@@ -1,16 +1,18 @@
-# function that estimates water supply from precipitation with parameters that vary to reflect water storage
-
 #' watersupply
 #'
 #' Calculate annual total precipitation each year for UCSB during a specified time period.
-#' @param UCSB_daily_rainfall data frame with columns station_id, water_year, year, month, day, daily_rain, code, station
-#' @param years the years in the data frame you want to specify for the time period (default is 1952 to 2019)
+#' @param data data frame with columns year, month, day, daily_rain
+#' @param years the years in the data frame you want to specify for the time period (default is 1952 to 2018)
+#' @param area the surface area of the reservoir
+#' @param total_depth the total depth of the reservoir
+#' @param start_depth the depth at the beginning of the year range in years
 #' @author Laura Ingulsrud & Keene Morrow
-#' @example annual_precip(UCSB_daily_rainfall, years = 2010:2015)
-#' @return Returns a dataframe containing,
+#' @example watersupply(data = UCSB_daily_rainfall, years = 2010:2015, area = 1000, total_depth = 2000, start_depth = 10)
+#' @return Returns a list containing,
 #' \describe{
-#'  \item{Year}{Years specified}
-#'  \item{Rainfall.inches}{The total precipitation (inches) for each year specified in the time period}
+#'  \item{end_vol}{Volume of water in reservoir at the end of the time period}
+#'  \item{pct_full}{Percent of reservoir volume occupied at the end of the time period}
+#'  \item{pct_change}{Percent change in reservoir volume occupied over the time period}
 #'  }
 #'
 
@@ -36,9 +38,6 @@ watersupply = function(data = "",  years = 1952:2018, area = 100, total_depth = 
 
   pct_change <- vol_change / tot_vol
 
-  return(list(pct_full, pct_change))
+  return(list(end_vol, pct_full, pct_change))
 
 }
-
-watersupply(data = UCSB_daily_rainfall)
-
