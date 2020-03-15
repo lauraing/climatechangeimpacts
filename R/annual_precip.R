0bfc6ea91e52f687f427cac1ea6112b0710f679f
Laura Ingulsrud <lgi@mac.com>
2020-03-14 17:35:42 -0700

    working on vignette

--- R/annual_precip.R
+++ R/annual_precip.R
@@ -1,10 +1,10 @@
 #' annual_precip
 #'
-#' Calculate annual total precipitation each year for UCSB during a specified time period.
-#' @param data data frame with columns year, month, day, daily_rain
-#' @param years the years in the data frame you want to specify for the time period (default is 1952 to 2019)
+#' Calculate annual total precipitation each year for a dataset during a specified time period.
+#' @param data dataframe with columns year, month, day, daily_rain
+#' @param years the years in the dataframe you want to specify for the time period (default is 1952 to 2019)
 #' @author Laura Ingulsrud & Keene Morrow
-#' @example annual_precip(UCSB_daily_rainfall, years = 2010:2015)
+#' @example annual_precip(data = UCSB_daily_rainfall, years = 2010:2015)
 #' @return Returns a dataframe containing,
 #' \describe{
 #'  \item{Year}{Years specified}
