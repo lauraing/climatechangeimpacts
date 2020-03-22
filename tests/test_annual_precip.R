c5dde1f036f9ca0e3c6883b815129457439c73ad
Keene Morrow <keene.morrow@gmail.com>
2020-03-22 15:24:26 -0700

    Test attempt for annual_precip()

--- tests/test_annual_precip.R
+++ tests/test_annual_precip.R
@@ -1,1 +1,18 @@
 # Test file for `annual_precip` function
+
+context("Annual Precipitation")
+
+library(tidyverse)
+
+test_data <- as.data.frame(cbind(year = c(2018, 2018, 2019, 2019, 2020, 2020),
+                                 month = c(1:6),
+                                 day = rep(1),
+                                 daily_rain = rep(1)))
+
+annual_precip(data = test_data, years = 2018:2020)
+
+test_that("annual_precip returns a data frame",{
+  expect_equal(class(annual_precip(data = test_data, years = 2018:2020)), "data.frame")
+})
+
+
--- tests/test_watersupply.R
+++ tests/test_watersupply.R
@@ -9,6 +9,15 @@ test_data <- as.data.frame(cbind(year = c(2018, 2018, 2019, 2019, 2020, 2020),
                                  day = rep(1),
                                  daily_rain = rep(1)))
 
+test_that("watersupply() returns a list",{
+  expect_equal(class(watersupply(data = test_data,
+                                 years = 2018:2020,
+                                 area = 1,
+                                 total_depth = 1,
+                                 start_depth = 0)), "list")
+})
+
+
 test_that("end volume (end_vol) returns expected value",{
   expect_equal(as.numeric(watersupply(data = test_data,
                                       years = 2018:2020,
