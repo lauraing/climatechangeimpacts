2c3e9dba395d1859f3483b194a36562183b1e12a
Laura Ingulsrud <lgi@mac.com>
2020-03-23 12:35:09 -0700

    added 3rd test for annual_precip

--- tests/testthat/test_annual_precip.R
+++ tests/testthat/test_annual_precip.R
@@ -15,9 +15,15 @@ test_that("annual_precip returns data frame",{
   expect_equal(class(annual_precip(data = test_data, years = 2018:2020)), "data.frame")
 })
 
-test_that("annual_precip returns expected values",{
+test_that("annual_precip returns expected rainfall values",{
   expect_equal(annual_precip(data = test_data, years = 2018:2020)$Rainfall.inches[1], 2)
   expect_equal(annual_precip(data = test_data, years = 2018:2020)$Rainfall.inches[2], 2)
   expect_equal(annual_precip(data = test_data, years = 2018:2020)$Rainfall.inches[3], 2)
   expect_equal(sum(annual_precip(data = test_data, years = 2018:2020)$Rainfall.inches), 6)
+})
+
+test_that("annual_precip returns expected years", {
+  expect_equal(annual_precip(data = test_data, years = 2018:2020)$Year[1], 2018)
+  expect_equal(annual_precip(data = test_data, years = 2018:2020)$Year[2], 2019)
+  expect_equal(annual_precip(data = test_data, years = 2018:2020)$Year[3], 2020)
 })
